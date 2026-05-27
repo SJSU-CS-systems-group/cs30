4e4cc95c5735f0ada28d6c0e1055acf71969fbea
Alder Wang <alderwang06@gmail.com>
2026-05-27 14:03:57 -0700

    error with creating table courses

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform).apply(false)
     alias(libs.plugins.composeCompiler).apply(false)
     alias(libs.plugins.kotlinSerialization).apply(false)
+    kotlin("jvm") version "2.0.21" apply false
+    kotlin("plugin.spring") version "2.0.21" apply false
+    kotlin("plugin.jpa") version "2.0.21" apply false
 }
